353467c2e065cfca3e511f2e4bd5ed3a9f3f9bdf
kevnade <kevlaspam@gmail.com>
2024-09-05 01:43:01 +1000

    redesign

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,14 @@ import { initializeApp, getApps, FirebaseApp } from 'firebase/app';
 import { getFirestore, Firestore } from 'firebase/firestore';
 import { getAuth, Auth } from 'firebase/auth';
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC9TzbHDjyhLMHUrmblyZLigE_s822RnrI",
-  authDomain: "fitspark-2139f.firebaseapp.com",
-  projectId: "fitspark-2139f",
-  storageBucket: "fitspark-2139f.appspot.com",
-  messagingSenderId: "907180730840",
-  appId: "1:907180730840:web:1abf4d1d76f2af3a4f6aad"
+  apiKey: "AIzaSyDGJqKkzdbC2NeI6hIPsPNNhAHuMutuDjE",
+  authDomain: "mygymgame.firebaseapp.com",
+  projectId: "mygymgame",
+  storageBucket: "mygymgame.appspot.com",
+  messagingSenderId: "388540703810",
+  appId: "1:388540703810:web:067d2fd683fcaca16fb9db"
 };
 
 let app: FirebaseApp;
